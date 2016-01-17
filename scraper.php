cf86d3c7b28c25c7436a7e3d6041733b89c28957
CJ Tully <cjtully14@gmail.com>
2016-01-17 11:09:24 -0500

    get city working

--- scraper.php
+++ scraper.php
@@ -1,9 +1,13 @@
 <?php
 
-$contents = file_get_contents("http://www.weather-forecast.com/locations/New-York/forecasts/latest");
+$city = $_GET['city'];
+
+$city = str_replace(" ", "", $city);
+
+$contents = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
 
 preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</s', $contents, $matches);
 
-print_r($matches);
+echo($matches[1]);
 
 ?>
